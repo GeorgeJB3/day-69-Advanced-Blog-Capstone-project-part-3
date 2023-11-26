12442dd212a4b98e9eea291d6649a19f07f06f49
George Baldwin <georgebaldwin@Georges-MacBook-Air.local>
2023-11-26 12:25:48 +0000

    added functionality to login() and added error handling to register()

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash
+from sqlalchemy import exc
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
@@ -9,7 +10,7 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 # Import your forms from the forms.py
-from forms import CreatePostForm, RegisterForm
+from forms import CreatePostForm, RegisterForm, LoginForm
 
 
 app = Flask(__name__)
@@ -18,7 +19,8 @@ ckeditor = CKEditor(app)
 Bootstrap5(app)
 
 # TODO: Configure Flask-Login
-
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 # CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
@@ -39,7 +41,7 @@ class BlogPost(db.Model):
 
 
 # TODO: Create a User table for all your registered users. 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
@@ -50,25 +52,56 @@ with app.app_context():
     db.create_all()
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    """Loads the user if the id exists in the database"""
+    return db.get_or_404(User, user_id)
+
+
 # TODO: Use Werkzeug to hash the user's password when creating a new user.
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        new_user = User(
-            name=form.name.data,
-            email=form.email.data,
-            password=form.password.data
-        )
-        db.session.add(new_user)
-        db.session.commit()
-    return render_template("register.html")
+    reg_form = RegisterForm()
+    try:
+        if reg_form.validate_on_submit():
+            hashed_salted_password = generate_password_hash(
+                reg_form.password.data,
+                method='pbkdf2:sha256',
+                salt_length=8
+            )
+            new_user = User(
+                name=reg_form.name.data,
+                email=reg_form.email.data,
+                password=hashed_salted_password
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            load_user(new_user)
+            return redirect(url_for('login'))
+    except exc.IntegrityError:
+        flash("You've already signed up with that email, login instead!")
+        return redirect(url_for("login"))
+    return render_template("register.html", form=reg_form)
 
 
 # TODO: Retrieve a user from the database based on their email. 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        email = login_form.email.data
+        password = login_form.password.data
+        user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+        if not check_password_hash(user.password, password):
+            flash("Password incorrect, please try again")
+            return redirect(url_for("login"))
+        elif not user:
+            flash("That email does not exist, please try again")
+            return redirect(url_for("login"))
+        else:
+            login_user(user)
+            return redirect(url_for("get_all_posts"))
+    return render_template("login.html", form=login_form)
 
 
 @app.route('/logout')
