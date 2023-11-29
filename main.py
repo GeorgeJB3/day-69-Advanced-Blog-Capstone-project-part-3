2de2b2b1654a2519d8a5888f9f4774220be57d2d
George Baldwin <georgebaldwin@Georges-MacBook-Air.local>
2023-11-29 10:13:00 +0000

    added comments to posts

--- main.py
+++ main.py
@@ -151,10 +151,23 @@ def get_all_posts():
 
 
 # TODO: Allow logged-in users to comment on posts
-@app.route("/post/<int:post_id>")
+@app.route("/post/<int:post_id>", methods=["GET", "POST"])
 def show_post(post_id):
     requested_post = db.get_or_404(BlogPost, post_id)
     comment_form = CommentForm()
+    if comment_form.validate_on_submit():
+        if not current_user.is_authenticated:
+            flash("You need to login or register to comment.")
+            return redirect(url_for("login"))
+
+        new_comment = Comment(
+            text=comment_form.comment.data,
+            comment_author=current_user,
+            parent_post=requested_post
+        )
+        db.session.add(new_comment)
+        db.session.commit()
+
     return render_template("post.html", post=requested_post, form=comment_form)
 
 
